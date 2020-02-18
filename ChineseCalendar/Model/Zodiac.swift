a04219d3c5bac1a628c8b2cbd8161fbf9c270c35
Salvador Sanchez Valls <salvadorsanchezvalls@gmail.com>
2020-02-18 09:36:37 +0000

    added Aspects, as well as ying/yang images for each aspect

--- ChineseCalendar/Model/Zodiac.swift
+++ ChineseCalendar/Model/Zodiac.swift
@@ -15,32 +15,33 @@ struct Zodiac {
     var element: Elements
     
     // MARK: - Text Variables
-    var aspect: String {
+    
+    var characteristics: String{
         switch animal {
         case .monkey:
-            return "Monkey: Early Fall. Minor Yang."
+            return "Independent, lively, ingenious, funny, daring, creative."
         case .rooster :
-            return "Roost: Mid-Fall. Mayor Yin."
+            return "Courageous, protector, beautiful, capable, communicative, honest."
         case .dog:
-            return "Dog: Late Fall. Minor Yang."
+            return "Loyal, responsible, sensible, moral, trustforthy, imaginative."
         case .pig:
-            return "Pig: Early Winter. Mayor Yin."
+            return "Sensual, generous, cheerful, tolerant, happy, enthusiast."
         case .rat:
-            return "Rat: Mid-Winter. Minor Yang."
+            return "Ingenious, ambitious, hard-working, purposeful, laborious, intelligent."
         case .ox:
-            return "Ox: Late Winter. Minor Yin."
+            return "Patient, corageous, conventional, trustworthy, determined, intelligent."
         case .tiger:
-            return "Tiger: Early Spring. Mayor Yang."
+            return "Daring, tiring, passionate, fast, dangerous, funny."
         case .hare:
-            return "Hare: Mid-Spring. Minor Yin."
+            return "Generous, intuitive, careful, selfish, discreet, sensible."
         case .dragon:
-            return "Dragon: Late Spring. Mayor Yang."
+            return "Enthusiast, daring, inspiring, winner, materialistic, independent."
         case .snake:
-            return "Snake: Early Summer. Minor Yin."
+            return "Intelligent, mysterious, intuitive, daring, orderly, sophisticated."
         case .horse:
-            return "Horse: Mid-Summer. Mayor Yang."
+            return "Loyal, hard-working, gregarious, cordial, energetic, popular."
         case .goat:
-            return "Goat: Late Summer. Minor Yin."
+            return "Pacific, adaptable, honest, creative, imaginative, sincere."
         }
     }
     
@@ -75,27 +76,87 @@ struct Zodiac {
         }
     }
     
-    // MARK: - Enums
-    enum Animals {
-        case monkey
-        case rooster
-        case dog
-        case pig
-        case rat
-        case ox
-        case tiger
-        case hare
-        case dragon
-        case snake
-        case horse
-        case goat
+    var aspect: (image: UIImage, text: String) {
+        switch animal {
+        case .monkey:
+            return (UIImage(named: "yang")!, "Early Fall. Minor Yang.")
+        case .rooster :
+            return (UIImage(named: "yin")!, "Mid-Fall. Major Yin.")
+        case .dog:
+            return (UIImage(named: "yang")!, "Late Fall. Minor Yang.")
+        case .pig:
+            return (UIImage(named: "yin")!, "Early Winter. Major Yin.")
+        case .rat:
+            return (UIImage(named: "yang")!, "Mid-Winter. Minor Yang.")
+        case .ox:
+            return (UIImage(named: "yin")!, "Late Winter. Major Yin.")
+        case .tiger:
+            return (UIImage(named: "yang")!, "Early Spring. Major Yang.")
+        case .hare:
+            return (UIImage(named: "yin")!, "Mid-Spring. Minor Yin.")
+        case .dragon:
+            return (UIImage(named: "yang")!, "Late Spring. Major Yang.")
+        case .snake:
+            return (UIImage(named: "yin")!, "Early Summer. Minor Yin.")
+        case .horse:
+            return (UIImage(named: "yang")!, "Mid-Summer. Major Yang.")
+        case .goat:
+            return (UIImage(named: "yin")!, "Late Summer. Minor Yin.")
+        }
     }
     
-    enum Elements{
-        case metal
-        case water
-        case earth
-        case wood
-        case fire
+    
+    var animalIcon: UIImage {
+        switch animal {
+        case .monkey:
+            return UIImage(named: "Monkey")!
+        case .rooster :
+            return UIImage(named: "Rooster")!
+        case .dog:
+            return UIImage(named: "Dog")!
+        case .pig:
+            return UIImage(named: "Pig")!
+        case .rat:
+            return UIImage(named: "Rat")!
+        case .ox:
+            return UIImage(named: "Ox")!
+        case .tiger:
+            return UIImage(named: "Tiger")!
+        case .hare:
+            return UIImage(named: "Hare")!
+        case .dragon:
+            return UIImage(named: "Dragon")!
+        case .snake:
+            return UIImage(named: "Snake")!
+        case .horse:
+            return UIImage(named: "Horse")!
+        case .goat:
+            return UIImage(named: "Goat")!
+        }
+    }
+    
+    
+    // MARK: - Enums
+    enum Animals : String {
+        case monkey     = "Monkey"
+        case rooster    = "Rooster"
+        case dog        = "Dog"
+        case pig        = "Pig"
+        case rat        = "Rat"
+        case ox         = "Ox"
+        case tiger      = "Tiger"
+        case hare       = "Hare"
+        case dragon     = "Dragon"
+        case snake      = "Snake"
+        case horse      = "Horse"
+        case goat       = "Goat"
+    }
+    
+    enum Elements : String{
+        case metal  = "Metal"
+        case water  = "Water"
+        case earth  = "Earth"
+        case wood   = "Wood"
+        case fire   = "Fire"
     }
 }
--- ChineseCalendar/ViewControllers/ResultsViewController.swift
+++ ChineseCalendar/ViewControllers/ResultsViewController.swift
@@ -14,13 +14,45 @@ class ResultsViewController: UIViewController {
     // MARK: - Outlets
     @IBOutlet weak var backgroundImage: UIImageView!
     
+    @IBOutlet weak var segControlZodiac: UISegmentedControl!
+    
+    @IBOutlet weak var lblAnimal: UILabel!
+    @IBOutlet weak var imgAnimalIcon: UIImageView!
+    
+    @IBOutlet weak var stackCharacteristics: UIStackView!
+    @IBOutlet weak var lblCharacteristics: UILabel!
+    
+    @IBOutlet weak var stackAspect: UIStackView!
+    @IBOutlet weak var lblAspect: UILabel!
+    @IBOutlet weak var imgAspect: UIImageView!
+    
+    @IBOutlet weak var stackElement: UIStackView!
+    @IBOutlet weak var lblElement: UILabel!
     
     // MARK: - Data Variables
     var zodiac: Zodiac!
     
     // MARK: - Actions
     @IBAction func typeChanged(_ sender: UISegmentedControl) {
-        print(sender.selectedSegmentIndex)
+        let index = sender.selectedSegmentIndex
+        
+        if index != 0 {
+            stackCharacteristics.isHidden = true
+        } else {
+            stackCharacteristics.isHidden = false
+        }
+        
+        if index != 2 {
+            stackAspect.isHidden = true
+        } else {
+            stackAspect.isHidden = false
+        }
+        
+        if index != 3 {
+            stackElement.isHidden = true
+        } else {
+            stackElement.isHidden = false
+        }
     }
     
     // MARK: - Lifecycle Functions
@@ -28,6 +60,14 @@ class ResultsViewController: UIViewController {
         super.viewDidLoad()
         
         backgroundImage.image = zodiac.elementBackground
+        lblAnimal.text = zodiac.element.rawValue + " " + zodiac.animal.rawValue
+        imgAnimalIcon.image = zodiac.animalIcon
+        lblCharacteristics.text = zodiac.characteristics
+        lblElement.text = zodiac.elementText
+        imgAspect.image = zodiac.aspect.image
+        lblAspect.text = zodiac.aspect.text
+        
+        typeChanged(segControlZodiac)
         
     }
 
